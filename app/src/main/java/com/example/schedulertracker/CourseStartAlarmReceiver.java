c7f1f3170ec6160f8562d9d1d07cf5eb77f9e118
aaronsamala <aaronsamala@gmail.com>
2019-03-16 23:03:41 +0300

    Fixed channel part of notifications.

--- app/src/main/java/com/example/schedulertracker/CourseStartAlarmReceiver.java
+++ app/src/main/java/com/example/schedulertracker/CourseStartAlarmReceiver.java
@@ -1,17 +1,21 @@
 package com.example.schedulertracker;
 
+import android.app.NotificationChannel;
 import android.content.BroadcastReceiver;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 
 public class CourseStartAlarmReceiver extends BroadcastReceiver {
+    String channel_id="course start";
     @Override
     public void onReceive(Context context, Intent intent) {
+        createNotificationChannel(context, channel_id);
         Intent notificationIntent = new Intent(context, CourseController.class);
 
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
@@ -20,7 +24,7 @@ public class CourseStartAlarmReceiver extends BroadcastReceiver {
 
         PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channel_id);
 
         Notification notification = builder.setContentTitle("SchedulerTracker Notification")
                 .setContentText("New course starting in 7 days")
@@ -30,5 +34,20 @@ public class CourseStartAlarmReceiver extends BroadcastReceiver {
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0, notification);
+
+
+    }
+    private void createNotificationChannel(Context context, String CHANNEL_ID) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = context.getResources().getString(R.string.channel_name_crs_start);
+            String description = context.getString(R.string.channel_description_crs_start);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
